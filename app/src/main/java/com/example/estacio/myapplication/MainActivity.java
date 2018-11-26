cbf443764ef9bc6586d10b38dcbbd955cef9facf
unknown <Estacio@MGJDFESL02012.estacio.acad>
2018-11-26 20:15:24 -0200

    Seek bar funcionando

--- app/src/main/java/com/example/estacio/myapplication/MainActivity.java
+++ app/src/main/java/com/example/estacio/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.estacio.myapplication;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,13 +13,17 @@ import java.text.ParseException;
 
 import faranjit.currency.edittext.CurrencyEditText;
 
-public class MainActivity extends AppCompatActivity {
+public class
+MainActivity extends AppCompatActivity {
 
     private CurrencyEditText mEditAlcool,mEditGasolina;
     private Button mBtn_Calc;
     private SeekBar seekBar;
     private TextView seekText,mChangeAlcool,mChangeGas,result;
     private Double Alcool,gas;
+    private SharedPreferences mPreference;
+    private SharedPreferences.Editor mEditor;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +42,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     calculate(mEditAlcool.getCurrencyDouble(),mEditGasolina.getCurrencyDouble());
+                    mEditor.putString("priceAlcool",mEditAlcool.getCurrencyText());
+                    mEditor.putString("priceGasolina",mEditGasolina.getCurrencyText());
+                    mEditor.commit();
 
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
             }
     });
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekText.setText( "R$" + progress*5);
-            mChangeAlcool.setText(progress*5);
-                mChangeGas.setText(progress*5);
+
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -56,12 +65,27 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                mChangeAlcool.setText("R$ "+seekBar.getProgress());
+                mChangeGas.setText("R$ "+seekBar.getProgress());
 
             }
 
 
         });
+
+
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mPreference = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreference.edit();
+
+        mEditAlcool.setText(mPreference.getString("priceAlcool","0.00"));
+        mEditGasolina.setText(mPreference.getString("priceGasolina","0.00"));
+    }
+
 
         private void calculate(double alcoolValue, double gasolinaValue) {
         result.setText("Alcool");
@@ -69,5 +93,7 @@ public class MainActivity extends AppCompatActivity {
             result.setText("Gasolina");
         }
     }
+
+
 
 }
